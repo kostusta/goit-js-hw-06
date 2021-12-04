ccf4f956497c3c256a60ecdd19bdfc9a5bc58e5b
Stanislav Kostiuchenko <kostiusta@gmail.com>
2021-12-04 20:25:04 +0200

    Adds second task answer

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,15 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const createIngredientsList = ingredients => {
+  return ingredients.map(ingredient => {
+    const ingredientItem = document.createElement('li');
+    ingredientItem.textContent = ingredient;
+    ingredientItem.classList.add('item');
+    return ingredientItem;
+  });
+};
+
+const ingredientsList = createIngredientsList(ingredients);
+
+const ingredientsList = document.getElementById('ingredients');
+ingredientsList.append(...ingredientsList);
