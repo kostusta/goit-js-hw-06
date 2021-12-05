d2491125fd1af1abfd29c6b19634787afa591619
Stanislav Kostiuchenko <kostiusta@gmail.com>
2021-12-05 11:52:27 +0200

    Adds fourth task answer

---
+++ js/task-04.js
@@ -0,0 +1,17 @@
+let counterValue = 0;
+
+const decrementBtnRef = document.querySelector('button[data-action="decrement"]');
+const incrementBtnRef = document.querySelector('button[data-action="increment"]');
+const valueRef = document.getElementById('value');
+
+const counterValueClickHandler = event => {
+  if (event.target.dataset.action === 'decrement') {
+    counterValue -= 1;
+  } else if (event.target.dataset.action === 'increment') {
+    counterValue += 1;
+  }
+  valueRef.textContent = counterValue;
+};
+
+decrementBtnRef.addEventListener('click', counterValueClickHandler);
+incrementBtnRef.addEventListener('click', counterValueClickHandler);
